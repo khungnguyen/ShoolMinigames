5ee96c0f1dc3e1bb628431c3d2b1114546835693
Quy Nguyen Duc <quy.nguyen-duc@ubisoft.com>
2023-04-05 14:24:45 +0700

    ! add spine animation + funciton for buttons

--- hanhtrinhlienlac/Assets/Scripts/CharSelectorUI.cs
+++ hanhtrinhlienlac/Assets/Scripts/CharSelectorUI.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 using Spine.Unity;
 public class CharSelectorUI : MonoBehaviour
 {
-    [SerializeField] SkeletonGraphic  _spine;
-    public void OnCharSelect(string id) {
+    [SerializeField] SkeletonGraphic _spine;
+    public void OnCharSelect(string id)
+    {
         _spine.Skeleton.SetSkin(id);
+        _spine.Skeleton.SetSlotsToSetupPose();
+        _spine.LateUpdate();
+         _spine.AnimationState.SetAnimation(0, "victory", false);
+         _spine.AnimationState.AddAnimation(0, "idle", true,0);
     }
 }
