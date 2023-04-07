2083b1a2694a59558f0b780b5281a315d64d37d6
hoang trantu <trantuhoang@gmail.com>
2023-04-07 23:16:32 +0700

    Ads click in Question game

---
+++ hanhtrinhlienlac/Assets/Scripts/AdsItemHandler.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class AdsItemHandler : MonoBehaviour, IPointerClickHandler
+{
+    [SerializeField] private string url;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Application.OpenURL(url);
+    }
+}
