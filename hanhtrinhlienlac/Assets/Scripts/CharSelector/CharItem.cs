292ce7bc9595705b3e079ab0722da0b80fd1771f
Quy Nguyen Duc <quy.nguyen-duc@ubisoft.com>
2023-04-07 20:29:32 +0700

    ! update charSelector

--- hanhtrinhlienlac/Assets/Scripts/CharSelector/CharItem.cs
+++ hanhtrinhlienlac/Assets/Scripts/CharSelector/CharItem.cs
@@ -10,9 +10,11 @@ public class CharItem : MonoBehaviour
     [SerializeField] Image _avatar;
     [SerializeField] TMP_Text _name;
     [SerializeField] Button _button;
+    [SerializeField] Sprite _highLight;
 
     private CharInfo _info;
     private Action<string> _listener;
+    private Sprite _temp;
     public void OnClick()
     {
         if (_listener != null)
@@ -24,17 +26,22 @@ public class CharItem : MonoBehaviour
     {
         _info = s;
         _name.SetText(_info.charName);
-        _button.transition = Selectable.Transition.SpriteSwap;
-        _button.targetGraphic.transform.GetComponent<Image>().sprite = _info.avatar;
-        _button.spriteState = new SpriteState {
-            highlightedSprite = _info.avatarSelected,
-            pressedSprite = _info.avatarSelected,
-            selectedSprite =_info.avatarSelected
-        };
+        _avatar.sprite = s.avatar;
+        _temp = _button.image.sprite;
+        // _button.transition = Selectable.Transition.SpriteSwap;
+        // _button.targetGraphic.transform.GetComponent<Image>().sprite = _info.avatar;
+        // _button.spriteState = new SpriteState {
+        //     highlightedSprite = _info.avatarSelected,
+        //     pressedSprite = _info.avatarSelected,
+        //     selectedSprite =_info.avatarSelected
+        // };
         return this;
     }
     public void SetClickListener(Action<string> ac)
     {
         _listener = ac;
+    }
+    public void OnSelected(bool select) {
+        _button.image.sprite = select?_highLight:_temp;
     }
 }
--- hanhtrinhlienlac/Assets/Scripts/CharSelector/CharSelectorUI.cs
+++ hanhtrinhlienlac/Assets/Scripts/CharSelector/CharSelectorUI.cs
@@ -10,36 +10,53 @@ public class CharSelectorUI : MonoBehaviour
     [SerializeField] SkeletonGraphic _spine;
     [SerializeField] List<CharInfo> _charInfoList;
     [SerializeField] TMP_Text _textDetailed;
-    private void Start() {
-        OnCharSelect("char_1");
-        _charInfoList.ForEach(e=>{
+
+    private string _curSelectedChar = "";
+    private void Start()
+    {
+        _charInfoList.ForEach(e =>
+        {
             e.uiItem.SetInfo(e).SetClickListener(OnCharSelect);
         });
+        OnCharSelect(UserInfo.GetInstance().GetSkin());
     }
+
     public void OnCharSelect(string id)
     {
         _spine.Skeleton.SetSkin(id);
         _spine.Skeleton.SetSlotsToSetupPose();
         _spine.LateUpdate();
-         _spine.AnimationState.SetAnimation(0, "victory", false);
-         _spine.AnimationState.AddAnimation(0, "idle", true,0);
-         UserInfo.GetInstance().SetSkin(id);
-        CharInfo info = _charInfoList.Find(e=>e.charId.Equals(id));
+        _spine.AnimationState.SetAnimation(0, "victory", false);
+        _spine.AnimationState.AddAnimation(0, "idle", true, 0);
+        UserInfo.GetInstance().SetSkin(id);
+        CharInfo info = _charInfoList.Find(e => e.charId.Equals(id));
         _textDetailed.SetText(info.info);
+        _charInfoList.ForEach(e =>
+        {
+            e.uiItem.OnSelected(e.charId.Equals(id));
+        });
+        _curSelectedChar = id;
     }
-    public void OnHide() {
+    public void OnHide()
+    {
         gameObject.SetActive(false);
     }
-    public void OnShow() {
+    public void OnShow()
+    {
         gameObject.SetActive(true);
+    }
+    public void OnHelp() {
+        CharInfo info = _charInfoList.Find(e => e.charId.Equals(_curSelectedChar));
+        Application.OpenURL(info.url);
     }
 }
 [System.Serializable]
-public struct CharInfo {
+public struct CharInfo
+{
     public string charId;
     public string charName;
     public string info;
     public Sprite avatar;
-    public Sprite avatarSelected;
     public CharItem uiItem;
+    public string url;
 }
