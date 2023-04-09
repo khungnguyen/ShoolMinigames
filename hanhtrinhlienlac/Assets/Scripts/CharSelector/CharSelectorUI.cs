77b80cda10ca80fcdcb619ede49ca46571da4c48
CitaTo <chithanhcm@gmail.com>
2023-04-09 14:54:06 +0700

    add sound to select char

--- hanhtrinhlienlac/Assets/Scripts/CharSelector/CharSelectorUI.cs
+++ hanhtrinhlienlac/Assets/Scripts/CharSelector/CharSelectorUI.cs
@@ -1,17 +1,24 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Spine.Unity;
 using TMPro;
 using UnityEngine.UI;
-using System;
 
 public class CharSelectorUI : MonoBehaviour
 {
+    [Serializable] struct AudioClips {
+        public AudioClip clickSFX;
+    }
+
     [SerializeField] SkeletonGraphic _spine;
     [SerializeField] List<CharInfo> _charInfoList;
     [SerializeField] TMP_Text _textDetailed;
 
+    [SerializeField] private SoundManager soundMgr;
+    
+    [SerializeField] private AudioClips audioClips;
     public Action<string> OnCharSelectListener;
 
     private string _curSelectedChar = "";
@@ -40,6 +47,8 @@ public class CharSelectorUI : MonoBehaviour
         });
         _curSelectedChar = id;
         OnCharSelectListener.Invoke(id);
+        
+        StartCoroutine(PlaySFXDelay(audioClips.clickSFX));
     }
     public void OnHide()
     {
@@ -52,6 +61,13 @@ public class CharSelectorUI : MonoBehaviour
     public void OnHelp() {
         CharInfo info = _charInfoList.Find(e => e.charId.Equals(_curSelectedChar));
         Application.OpenURL(info.url);
+    }
+    
+    private IEnumerator PlaySFXDelay(AudioClip ac)
+    {
+        //Thanh.To cheat delay to avoid error at first frame
+        yield return new WaitForEndOfFrame();
+        soundMgr.PlaySfx(ac);
     }
 }
 [System.Serializable]
